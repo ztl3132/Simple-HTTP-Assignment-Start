1f32d14f42b6d054178ff9759d96daf24ae59815
ztl3132 <ztl3132@rit.edu>
2023-09-08 17:53:27 -0400

    Filled out htmlResponses.js and updated server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,18 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
