4a027976074601d75de80fdbea73ceef1c3803c3
ztl3132 <ztl3132@rit.edu>
2023-09-08 18:07:11 -0400

    Added switch stuff to server.js and fixed bugs

--- src/server.js
+++ src/server.js
@@ -6,21 +6,33 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-    console.log(request.url);
+  console.log(request.url);
 
-    switch (request.url) {
-        case '/':
-            htmlHandler.getIndex(request, response);
-            break;
-        case '/page2':
-            htmlHandler.getPage2(request, response);
-            break;
-        default:
-            htmlHandler.getIndex(request, response);
-            break;
-    }
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1${port}`);
+  console.log(`Listening on 127.0.0.1${port}`);
 });
