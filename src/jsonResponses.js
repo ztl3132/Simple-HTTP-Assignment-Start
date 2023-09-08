22b29d5dcfdcaf13e5c8230796dd23ce708cb7d5
ztl3132 <ztl3132@rit.edu>
2023-09-08 18:01:24 -0400

    Filled out jsonResponses.js

---
+++ src/jsonResponses.js
@@ -0,0 +1,28 @@
+const text = require('./textResponses.js');
+
+const getHelloJSON = (request, response) => {
+    const helloJSON = {
+        message: text.hello,
+    };
+    const stringMessage = JSON.stringify(helloJSON);
+
+    response.writeHead(200, { 'Content-Type': 'application/JSON' });
+    response.write(stringMessage);
+    response.end;
+};
+
+const getTimeJSON = (request, response) => {
+    const timeJSON = {
+        time: time.getTimeString(),
+    };
+    const stringMessage = JSON.stringify(timeJSON);
+
+    response.writeHead(200, { 'Content-Type': 'application/JSON' });
+    response.write(stringMessage);
+    response.end;
+};
+
+module.exports = {
+    getHelloJSON,
+    getTimeJSON
+};
