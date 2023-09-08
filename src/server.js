378e47be2d6778e32bc1678f314836b4aca1278a
ztl3132 <ztl3132@rit.edu>
2023-09-08 17:58:22 -0400

    Filled out textResponses.js and formatted others

--- src/server.js
+++ src/server.js
@@ -6,21 +6,21 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
+    console.log(request.url);
 
-  switch (request.url) {
-    case '/':
-      htmlHandler.getIndex(request, response);
-      break;
-    case '/page2':
-      htmlHandler.getPage2(request, response);
-      break;
-    default:
-      htmlHandler.getIndex(request, response);
-      break;
-  }
+    switch (request.url) {
+        case '/':
+            htmlHandler.getIndex(request, response);
+            break;
+        case '/page2':
+            htmlHandler.getPage2(request, response);
+            break;
+        default:
+            htmlHandler.getIndex(request, response);
+            break;
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1${port}`);
+    console.log(`Listening on 127.0.0.1${port}`);
 });
---
+++ src/textResponses.js
@@ -0,0 +1,26 @@
+const hello = 'Hello World';
+
+const getTimeString = () => {
+    const d = new Date();
+    const dateString = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds}`;
+    return dateString;
+};
+
+const getTime = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(getTimeString());
+    response.end();
+};
+
+const getHello = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(hello);
+    response.end();
+};
+
+module.exports = {
+    hello,
+    getTimeString,
+    getHello,
+    getTime
+};
