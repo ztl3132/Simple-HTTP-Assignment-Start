c2c6170cbb485a7cda534fb743a85b58b0a02e31
ztl3132 <ztl3132@rit.edu>
2023-09-08 17:45:51 -0400

    Added initial server.js code

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1${port}`);
+});
